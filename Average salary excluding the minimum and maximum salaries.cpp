3a50568c95b8d85dc3c2b23c22bbac6eaa1516e7
Gaurav Bisht <77239173+Gauravbisht231@users.noreply.github.com>
2023-05-01 13:17:41 +0530

    Update Average salary excluding the minimum and maximum salaries.cpp

--- Average salary excluding the minimum and maximum salaries.cpp
+++ Average salary excluding the minimum and maximum salaries.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     double average(vector<int>& s) {
-        double res=0;
-        sort(s.begin(), s.end());
-        int n= s.size();
+    double res=0;
+int mini= 1e6;
+int maxi= 0;
+int n= s.size();
 
-        for(int i=1; i<n-1; i++){
-            res+=s[i];
-        }
-        // O(NlogN) approach
-        res= double(res)/(n-2);
-        return res;
+for(int i=0; i<n; i++){
+        mini= min(mini, s[i]);
+        maxi= max(maxi, s[i]);
+        res+= s[i];
+}
+res= res- mini- maxi;
+res= double(res)/(n-2);
+return res;
+// O(N) approach
     }
 };
