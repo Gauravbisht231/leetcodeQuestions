1c3306df64841b30c74d50a27ac12f24eb47b248
Gaurav Bisht <77239173+Gauravbisht231@users.noreply.github.com>
2023-10-04 18:22:09 +0530

    Create Design HashMap.cpp
    Design a HashMap without using any built-in hash table libraries.
    
    Implement the MyHashMap class:
    
    MyHashMap() initializes the object with an empty map.
    void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
    int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
    void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
     
    
    Example 1:
    
    Input
    ["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
    [[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
    Output
    [null, null, null, 1, -1, null, 1, null, -1]

---
+++ Design HashMap.cpp
@@ -0,0 +1,53 @@
+lass MyHashMap {
+    //          using custom Hashfunction-- bucket method
+public:
+vector<list<pair<int, int>>> buck;
+    MyHashMap() {
+        
+        buck.resize(10000);
+    }
+    
+    void put(int key, int value) {
+     int custom_ind= key%10000;
+        auto & chain= buck[custom_ind]; 
+        for(auto &it: chain){
+            if(it.first==key) {
+                it.second= value;
+                return;
+            }
+        } 
+        chain.emplace_back(key, value);  
+        return;
+    }
+    
+    int get(int key) {
+        int custom_ind= key%10000;
+        auto & chain= buck[custom_ind];
+        for(auto it: chain){
+            if(it.first==key){
+                return it.second;
+            }
+        }
+        return -1;
+    }
+    
+    void remove(int key) {
+        int custom_ind= key%10000;
+        auto &chain = buck[custom_ind];
+        for( auto it= chain.begin(); it!= chain.end(); it++){
+            if(it->first==key){
+                chain.erase(it);
+                return;
+            }
+            else continue;
+        }
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
