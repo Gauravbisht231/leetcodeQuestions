01bdcbac0ec8afbf277dd7f2cb08ff4b41124ea0
Gaurav Bisht <77239173+Gauravbisht231@users.noreply.github.com>
2023-10-10 19:43:46 +0530

    Update Find First and Last Position of Element in Sorted Array.cpp

--- Find First and Last Position of Element in Sorted Array.cpp
+++ Find First and Last Position of Element in Sorted Array.cpp
@@ -1,29 +1,66 @@
 class Solution {
-    //                            BRUTE FORCE
-    //         O(N) time and Constant space complexity         
+    //                            tRYING OPTIMISING IT: BINARY SEARCH
+    //                       O(log(n)) time and Constant space complexity     
+
+private:
+int Bsfirst(vector<int> &nums, int target, int n){
+    int ind=-1;
+    int lo=0, hi= n-1;
+    while(lo<=hi)
+    {
+        int mid= lo+(hi-lo)/2;
+        if(nums[mid]== target){
+            // if we have reached out to first index
+            if(mid==0 or nums[mid-1]!=target){
+                    ind= mid;
+                    break;
+            }
+            //  else keep on searching in the  left half
+            else {
+                hi= mid-1;
+            } 
+        }
+        else if(nums[mid]< target){
+            lo= mid+1;
+        }
+        else hi= mid-1;
+    }
+    return ind;
+
+}   
+int BsSecond(vector<int> &nums, int target, int n){
+    int second=-1;
+    int lo=0, hi= n-1;
+    while(lo<=hi){
+        int mid= lo+(hi-lo)/2;
+        if(nums[mid]== target){
+            // if we have reached out to Last index of occurence
+            if(mid==n-1 or nums[mid+1]!=target){
+                second= mid;
+                break;
+            }
+            // else keep on finding the last index in the right half
+            else {
+                lo= mid+1;
+            }
+        }
+        else if(nums[mid]<target){
+            lo= mid+1;
+        }
+        else hi= mid-1;
+    }
+    return second;
+} 
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int n= nums.size();
         int first=-1, second=-1;
         vector<int> res(2,-1);
-        // res.push_back(first);
-        // res.push_back(second);
-
-        for(int i=0;i<n; i++){
-            if(nums[i]==target) {
-                first=i;
-                break;
-            }
-        }
-        for(int i=n-1; i>=0; i--){
-            if(nums[i]==target){
-                second=i;
-                break;
-            }
-        }
-        res[0]=first;
-        res[1]=second;
+        int lo=0, hi= n-1;
+        first= Bsfirst(nums, target, n);
+        second= BsSecond(nums, target, n);
+        res[0]= first;
+        res[1]= second;
         return res;
-
     }
 };
